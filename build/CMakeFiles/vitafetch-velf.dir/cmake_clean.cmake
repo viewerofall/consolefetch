8b63aea8f5ba9ccce8ec959cea1e30d981705917
joemo <123335711+viewerofall@users.noreply.github.com>
2026-03-08 13:45:51 -0700

    Delete build directory

--- build/CMakeFiles/vitafetch-velf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vitafetch-velf"
-  "vitafetch.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vitafetch-velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
